1aef81f69df524666a84359675da60e08cc5af0e
wilsonjacobsen <Pipoca1992@>
2019-12-28 08:56:40 -0300

    init

---
+++ src/main/java/com/sicredi/votingsessionservice/VotingSessionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sicredi.votingsessionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSessionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingSessionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sicredi/votingsessionservice/VotingSessionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicredi.votingsessionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSessionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
